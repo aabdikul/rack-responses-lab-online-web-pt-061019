ee6a69c27d2eeac2b16c73fae2bac4dad5c1c6ef
Aida Abdikulova <abdikulova@gmail.com>
2019-10-06 23:50:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     if time.hour < 12
       resp.write "Good Morning!"
     else
-      resp.write "Good Evening!"
+      resp.write "Good Afternoon!"
     end
     resp.finish
   end
