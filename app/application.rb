bca52ff0bcb004d003f060222e931d041f1c8b65
Aida Abdikulova <abdikulova@gmail.com>
2019-10-06 23:58:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    if Time.new(2015,11,27,9,30).hour.to_i < 13
+    if Time.new(env).hour.to_i < 13
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
