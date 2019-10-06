291497740ff66d5a8794604619dd184dbdd01b83
Aida Abdikulova <abdikulova@gmail.com>
2019-10-06 23:46:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    time = Time.strftime("%k:%M")
+    time = Time.new.strftime("%k:%M")
     resp.write "#{time}"
     resp.finish
   end
